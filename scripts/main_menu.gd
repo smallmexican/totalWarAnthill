dc816bd3d5be2aa6374f521c629773aee494f33b
thesmallmexican <thesmallmexican@gmail.com>
2025-07-02 21:52:09 +0100

    updated MainMenu scene and script

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,10 +1,12 @@
 extends Control
 
-func _on_Start_pressed():
+func _on_start_button_pressed() -> void:
 	get_tree().root.get_node("Main").start_game()
 
-func _on_Quit_pressed():
-	get_tree().quit()
 
-func _on_Settings_pressed():
+func _on_settings_button_pressed() -> void:
 	get_tree().root.get_node("Main").load_menu("res://scenes/ui/SettingsMenu.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
